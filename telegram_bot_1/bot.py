6e43e8338d6c0e0bb0ea58c1fb00cc8776404016
Evgeniy Arkchipov <spbrba@rambler.ru>
2022-03-16 17:18:52 +0300

    '16.03_Job'

--- telegram_bot_1/bot.py
+++ telegram_bot_1/bot.py
@@ -1,17 +1,38 @@
 import telebot
 import configure
 
-TOKEN = configure.config['token']
+TOKEN = str(configure.config['token'])
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(content_types = ['text','photo']) #.message_handler - обработчик поступающих сообщщений
-def function_name(message):
-    if message.text == True:
-        if message.text.lower() in ['привет','приветик']:
-            bot.reply_to(message, "Привет, Пирожуля!)")
-        else:
-            bot.reply_to(message,"Нужно поздороваться")
-    elif message. :
-        bot.send_message(message, "Красивый голос")
 
-bot.polling(none_stop=True) #запуск постоянной работы
+@bot.message_handler(commands=['start'])
+def comm(message):
+    bot.send_message(message.chat.id, 'Приветствие от /start')
+    f = open('users.txt', 'a')
+    f.write(message.chat.username+'\n'+str(message.chat.id)+'\n')
+    f.close()
+
+@bot.message_handler(content_types = ['text'])#.message_handler - обработчик поступающих сообщщений
+def function_name(message):
+    if message.text.lower() in ['привет','приветик']:
+        bot.send_message(message.chat.id, "Привет, Пирожуля!)")
+    else:
+            bot.send_message(message.chat.id, "Нужно поздороваться")
+
+@bot.message_handler(content_types = ['photo'])#.message_handler - обработчик поступающих сообщщений
+def function_name(message):
+            bot.send_message(message.chat.id, "Это фото!)")
+
+#@DECOR()
+#def dobavlenie():
+#    f = open('users.txt', 'a')
+#    f.write(message.chat.username+'\n'+str(message.chat.id)+'\n')
+#    f.close()
+
+
+
+bot.polling(none_stop=True)  # запуск постоянной работы
+
+#    elif message:
+#       bot.send_message(message, "Красивый голос")
+
--- telegram_bot_1/configure.py
+++ telegram_bot_1/configure.py
@@ -2,3 +2,10 @@ config = {
     'name': 'Example_march_bot',
     'token': '5238230851:AAFXn3Qwzt5WbxZIgHwnMP6x1dOCe-ZupIM'
 }
+
+
+
+
+
+
+
